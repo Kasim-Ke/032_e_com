59a8c9cda43e13fe5625266c8ac19fedf05bcf93
Fermented-coder <kuivalappa@gmail.com>
2025-01-04 17:55:01 +0200

    hr fixed on lgsize

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -6,8 +6,8 @@ import heroMobile from "../assets/home/mobile/image-header.jpg";
 const HomeHero = () => {
   return (
     <div className=" relative lg:max-w-[1440px] md:max-w-[768px] max-w-[375px] mx-auto flex items-center justify-center  ">
-      <div className="relative ">
-        <hr className="absolute lg:w-[1110px] md:w-[689px] lg:left-[96px] top-[90px] md:left-[39px] w-full border-t border-[#979797] opacity-20" />
+      <div className="relative mx-auto ">
+        <hr className="absolute lg:w-[1110px] md:w-[689px] lg:left-[165px] top-[90px] md:left-[39px]  w-full border-t border-[#979797] opacity-20" />
 
         <img
           className="object-cover lg:block hidden"
